a5d8cf65b7f072d640c74c0aeccec81eb21008a9
Farazdaq <frzdg6@gmail.com>
2025-12-05 15:38:29 +0400

    profile comp added

---
+++ weather-electricity-analytics-system-dashboard/src/components/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import "./Profile.css";
+import theme from "../../assets/styles/theme";
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Profile Image"
+        className="profile-image"
+      />
+    </div>
+  );
+}
+
+export default Profile;
--- weather-electricity-analytics-system-dashboard/src/layouts/Header/Header.tsx
+++ weather-electricity-analytics-system-dashboard/src/layouts/Header/Header.tsx
@@ -1,6 +1,7 @@
 import theme from "../../assets/styles/theme";
 import "./Header.css";
 import Search from "../../components/Search/Search";
+import Profile from "../../components/Profile/Profile";
 
 function Header() {
   return (
@@ -10,8 +11,9 @@ function Header() {
         backgroundColor: theme.colors.primaryTransparent,
       }}
     >
-      <div>
+      <div className="header-column-container">
         <Search />
+        <Profile />
       </div>
     </header>
   );
